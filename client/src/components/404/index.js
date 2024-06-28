adb1ba392dcb293fdbbffb63ef5a9c8844055a0c
ciatph <ciat.ph@gmail.com>
2024-06-28 16:16:26 +0800

    chore: fireworks on the 404 page

--- client/src/components/404/index.js
+++ client/src/components/404/index.js
@@ -1,6 +1,39 @@
+import { useEffect, useRef } from 'react'
+import { Fireworks } from '@fireworks-js/react'
+
+import styles from './styles.module.css'
+
 function NotFound () {
+  const mounted = useRef(false)
+
+  useEffect(() => {
+    console.log('--mounted', mounted.current, mounted)
+    mounted.current = true
+
+    return () => {
+      mounted.current = false
+    }
+  }, [])
+
   return (
-    <h2>Page not found</h2>
+    <div className={styles.container}>
+      <div styles={styles.fireworks}>
+        <Fireworks
+          ref={mounted}
+          options={{ opacity: 0.5 }}
+          style={{
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '320px'
+          }}
+        />
+      </div>
+
+      <div className={styles.centered}>
+        <h1>Page not found</h1>
+      </div>
+    </div>
   )
 }
 
